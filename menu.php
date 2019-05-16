8b222484518eab32aaa63c83ad21bdfe5fd89f53
sevvalp <38830988+sevvalp@users.noreply.github.com>
2019-05-16 10:04:18 +0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,5 @@
+<ul>
+  <li><a href="index.php">Home sweet home</a></li>
+  <li><a href="index.php">Do you want to search for courses?</a></li>
+  <li><a href="comment.php">Wanna comment on the courses?</a></li>
+</ul>
